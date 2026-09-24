f541053967782ef2a34449dd4719020f53749c0d
Jenna Coates <jennacoates04@gmail.com>
2026-09-23 23:00:48 -0600

    basic structure and validation working, need to add advaced validation and view on event page

--- dmit2015-assignment01-jennacoates/src/main/java/dmit/view/CampusEvent.java
+++ dmit2015-assignment01-jennacoates/src/main/java/dmit/view/CampusEvent.java
@@ -1,18 +1,62 @@
 package dmit.view;
 
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.faces.application.FacesMessage;
+import jakarta.faces.context.FacesContext;
+import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Named;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
 @Named
-@RequestScoped
-public class CampusEvent {
+@ViewScoped
+public class CampusEvent implements Serializable {
+
+    private int submssionCount = 2601;
+
+    public int getSubmssionCount() {
+        return submssionCount;
+    }
+
     private int eventId;
+
+    @NotBlank(message = "Please give a name for the event!")
     private String eventName;
+
+    @NotBlank(message = "Please provide a name to the form!")
     private String organizerName;
+
+    @NotBlank(message = "Please set a date for the event!")
     private LocalDate eventDate;
+
+    @NotNull(message = "Please specify your events capacity!")
     private int capacity;
+
+    public void submit(){
+        submssionCount++;
+        String messageDetail = String.format("Event Name:%s, Organizer Name: %s Event Date:%s Capacity:%s EventId:%s",
+                eventName,
+                organizerName,
+                eventDate,
+                capacity,
+                eventId
+        );
+
+        FacesMessage message = new FacesMessage(
+                FacesMessage.SEVERITY_INFO,
+                "Form Submitted",
+                messageDetail
+        );
+        FacesContext.getCurrentInstance()
+                .addMessage(null,message);
+        eventName=null;
+        organizerName=null;
+        eventName=null;
+        capacity=0;
+    }
 
     public int getEventId() {
         return eventId;
